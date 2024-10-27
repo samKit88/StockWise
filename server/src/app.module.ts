2c3e78ef7d2c221d674a5c825f6668767603f559
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-27 16:12:09 +0300

    project setup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,47 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MailModule } from './mail/mail.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import config from './config/config';
+import { ConfigService } from '@nestjs/config';
+import { MailService } from './mail/mail.service';
+// import { InventoryModule } from './inventory/inventory.module';
+import { AuthGuard } from './guards/auth.guards';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    // InventoryModule,
+    PrismaModule,
+
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [config],
+    }),
+    MailModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (config) => ({
+        secret: config.get('jwt.secret'),
+      }),
+      global: true,
+      inject: [ConfigService],
+    }),
+    // InventoryModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+
+    // {
+    //   provide: 'APP_GUARD',
+    //   useClass: AuthGuard,
+    // },
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const loggerInstance = app.get(ConfigService);
+  const config = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(config.get('port') as number);
+  console.debug(`server started at port ${config.get('port') as number}`);
 }
 bootstrap();
