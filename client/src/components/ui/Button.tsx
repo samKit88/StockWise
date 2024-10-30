2144cfd6af4740029629c40e5c0afadc75623a9a
samKit88 <samuel.girma.cx9896@gmail.com>
2024-10-30 09:45:56 +0300

    Add feature

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonPro {
 const ButtonComponent = ({ isLoading, buttonName }: ButtonPro) => {
   return (
     <Button
-      className="mx-auto w-28 mt-6 py-2 rounded-lg"
+      className="mx-auto w-32 mt-6 py-2 rounded-lg"
       type="submit"
       fullWidth
       loading={isLoading}
--- client/src/components/ui/Form.tsx
+++ client/src/components/ui/Form.tsx
@@ -1,50 +1,221 @@
 import { type UseFormReturnType } from '@mantine/form'
 
-import React from 'react'
-import { Card } from '@mantine/core'
-import { SigninForm } from '../../schema/SigninSchema'
-import { SignupForm } from '../../schema/SignupSchema'
-import ButtonComponent from './Button'
-import TextInputComponent from './TextInput'
+import React, { useState } from 'react'
+import {
+  ActionIcon,
+  Autocomplete,
+  Box,
+  Button,
+  Card,
+  Divider,
+  Group,
+  NativeSelect,
+  NumberInput,
+  PasswordInput,
+  Select,
+  Switch,
+  Table,
+  TableTd,
+  TableTh,
+  TableTr,
+  Text,
+  TextInput,
+} from '@mantine/core'
+import { DateInput } from '@mantine/dates'
+import { addSalesConfig, FormFiled } from '../../types/FormFileds'
+import { buildCreateSlice } from '@reduxjs/toolkit'
+import { ItmesFormData, SalesFormData } from '../../schema/salesSchema'
+import { IoContractSharp } from 'react-icons/io5'
 
-interface SigninFormProps {
-  form: UseFormReturnType<SigninForm>
-  onSave: (values: SigninForm) => void
+// interface FormValues {
+//   salesItems: ItmesFormData
+// }
+
+interface FormWithSubmit {
+  onSubmit: (data: any) => { () }
+  getInputProps: (name: string) => any
+  getValues(): () => SalesFormData
+  insertListItem(data: any, field: any): () => any
+  removeListItem(data: any, index: any): () => any
+  key(data: any): () => any
+}
+
+interface FormDataProps<T> {
+  form: T
+  onSave: (values: T) => void
   isLoading: boolean
   type: string
+  fields: FormFiled[]
 }
 
-interface SignupFormProps {
-  form: UseFormReturnType<SignupForm>
-  onSave: (values: SignupForm) => void
-  isLoading: boolean
-}
-
-const AuthForm: React.FC<SigninFormProps> = ({
+const AuthForm = <T extends FormWithSubmit>({
   form,
   onSave,
   isLoading,
   type,
-}) => {
+  fields,
+}: FormDataProps<T>) => {
+  const [boxButtonClicked, setBoxButtonClicked] = useState<number[]>([1])
+  const [value, setValue] = useState<Date | null>(null)
+
+  const config = addSalesConfig()
+  const handleSubmit = (data) => {
+    // console.log(data)
+    const values: T = {} as T
+    onSave(data) // Call onSave function passed as prop
+  }
+
+  const handleBoxButtonClick = () => {
+    setBoxButtonClicked((value) => [...value, 1]),
+      form.insertListItem('salesItems', fields)
+  }
+
+  const handleRemoveItem = () => {
+    setBoxButtonClicked((value) =>
+      value.length > 1 ? value.slice(0, -1) : value
+    )
+  }
+
+  const addBoxFileds = boxButtonClicked.map((item, index) => (
+    <Group key={item} mt="xs">
+      <NativeSelect
+        placeholder="John Doe"
+        withAsterisk
+        style={{ flex: 1 }}
+        key={form.key(`salesItems.${index}.name`)}
+        {...form.getInputProps(`salesItmes.${index}.name`)}
+      />
+      <NumberInput
+        placeholder="John Doe"
+        withAsterisk
+        style={{ flex: 1 }}
+        key={form.key(`salesItems.${index}.name`)}
+        {...form.getInputProps(`salesItmes.${index}.name`)}
+      />
+
+      <NumberInput
+        placeholder="John Doe"
+        withAsterisk
+        style={{ flex: 1 }}
+        key={form.key(`salesItems.${index}.name`)}
+        {...form.getInputProps(`salesItmes.${index}.name`)}
+      />
+      <ActionIcon color="red" onClick={handleRemoveItem}>
+        <IoContractSharp size="1rem" />
+      </ActionIcon>
+    </Group>
+  ))
+
   return (
     <>
       <Card className="mb-4">
         <form
           className=" px-3 py-2 border border-gray-150 rounded-lg "
-          onSubmit={form.onSubmit(onSave)}
+          onSubmit={form.onSubmit(handleSubmit)}
         >
-          {' '}
-          {type === 'signin' ? (
-            <>
-              <TextInputComponent form={form} type="signin" />
-              <ButtonComponent isLoading={isLoading} buttonName="Sign In" />
-            </>
-          ) : (
-            <>
-              <TextInputComponent form={form} type="signup" />
-              <ButtonComponent isLoading={isLoading} buttonName="SignUp" />
-            </>
-          )}
+          <div className="flex flex-wrap gap-6 ">
+            {fields.map((field, index) => (
+              <div
+                key={field.name}
+                className={`${
+                  field.type === 'box'
+                    ? 'w-full'
+                    : 'w-auto flex-1 min-w-[300px] '
+                }`}
+              >
+                {field.type === 'select' ? (
+                  <NativeSelect
+                    label={field.label}
+                    {...form.getInputProps(field.name)}
+                    data={field.options}
+
+                    // mt="md"
+                  />
+                ) : field.type === 'text' ? (
+                  <TextInput
+                    label={field.label}
+                    type={field.type}
+                    {...form.getInputProps(field.name)}
+                  />
+                ) : field.type === 'number' ? (
+                  <NumberInput
+                    label={field.label}
+                    placeholder="quantity"
+                    {...form.getInputProps(field.name)}
+                  />
+                ) : field.type === 'password' ? (
+                  <PasswordInput
+                    // className="pt-3 flex flex-col gap-6 text-gray-700"
+                    label={field.label}
+                    placeholder="use strong password"
+                    {...form.getInputProps(field.name)}
+                    withAsterisk
+                  />
+                ) : field.type === 'date' ? (
+                  <DateInput
+                    valueFormat="YYYY/MM/DD"
+                    onChange={setValue}
+                    label="Date input"
+                    placeholder="Date input"
+                    {...form.getInputProps(field.name)}
+                  />
+                ) : field.type === 'box' ? (
+                  <>
+                    {field.boxFields?.map((boxField, index) => (
+                      <Box className="w-full">
+                        <>
+                          <p>asdfkjaskdfj;kajsd;fkj;aksdjfkjskdfjkjsdkfj</p>
+                          {/* {boxButtonClicked.length > 0 ? (
+                            <Group mb="xs" className="flex justify-evenly">
+                              <Text>{boxField.name}</Text>
+                              <Text>{boxField.name}</Text>
+                              <Text>{boxField.name}</Text>
+                            </Group>
+                          ) : (
+                            <TextInput
+                              placeholder="John Doe"
+                              withAsterisk
+                              style={{ flex: 5 }}
+                              key={form.key(`salesItems.${index}.name`)}
+                              {...form.getInputProps(
+                                `salesItems.${index}.name`
+                              )}
+                              {...form.getInputProps(
+                                `salesItems.${index}.name`
+                              )}
+                            />
+                          )}
+
+                          {addBoxFileds} */}
+                          <Group justify="center" mt="md">
+                            <Button onClick={handleBoxButtonClick}>
+                              Add Item
+                            </Button>
+                          </Group>
+                        </>
+                      </Box>
+                    ))}
+                  </>
+                ) : (
+                  <Autocomplete
+                    label={field.label}
+                    data={field.options || []}
+                    {...form.getInputProps(field.name)}
+                  />
+                )}
+              </div>
+            ))}
+          </div>
+          <Button
+            type="submit"
+            className="mt-6 mx-auto block"
+            // loading={isUpdating}
+            loaderProps={{
+              type: 'dots',
+            }}
+          >
+            Save
+          </Button>
         </form>
       </Card>
     </>
