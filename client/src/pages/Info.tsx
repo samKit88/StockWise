2d5c7313746953fc048de76d25ae5a46d6046ae1
samKit88 <samuel.girma.cx9896@gmail.com>
2024-11-01 09:51:46 +0300

    Add feature

---
+++ client/src/pages/Info.tsx
@@ -0,0 +1,13 @@
+import NavGrid from '../components/ui/NavGrid'
+
+const Info = () => {
+  return (
+    <div className="mx-8">
+      <div className="h-24">
+        <NavGrid />
+      </div>
+    </div>
+  )
+}
+
+export default Info
