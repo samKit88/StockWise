483343d0abceea539adba4ce160cae2ccd59f95d
samKit88 <samuel.girma.cx9896@gmail.com>
2024-11-16 15:05:42 +0300

    Add feature

--- client/src/services/shared/sales.query.ts
+++ client/src/services/shared/sales.query.ts
@@ -1,9 +1,17 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
 import { AxiosError, AxiosResponse } from 'axios'
-import { createSales, fetchSales } from '../api/salesApi'
+import { createSales, fetchSales, fetchSalesById } from '../api/salesApi'
 
 export const fetchSalesQuery = (queryKey: string[]) => {
   return useQuery({ queryKey, queryFn: fetchSales })
+}
+
+export const fetchSalesByIdQuery = (id: string) => {
+  // console.log('fetch By id', id)
+
+  return useQuery(['fetchById', id], () => fetchSalesById(id), {
+    enabled: !!id,
+  })
 }
 
 export const createSalesMutation = (
