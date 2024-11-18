676282d7e0abf3c9c3b5dcb6fd02486c87d8939a
samKit88 <samuel.girma.cx9896@gmail.com>
2024-11-18 16:49:12 +0300

    Add feature

--- client/src/components/DeleteInventoryModal.tsx
+++ client/src/components/DeleteInventoryModal.tsx
@@ -1,6 +1,6 @@
 import { Button, Grid, Modal, SimpleGrid, Text } from '@mantine/core'
 
-const DeleteInventory = ({ onDeleteButton, onClose }) => {
+const DeleteInventory = ({ onDeleteButton, actionModalClose, deleteData }) => {
   return (
     <>
       <SimpleGrid cols={1} className="gap-8 pt-5">
@@ -8,10 +8,14 @@ const DeleteInventory = ({ onDeleteButton, onClose }) => {
           <Text size="md">Are you sure you want to delete?</Text>
         </div>
         <div className="flex justify-end gap-4">
-          <Button onClick={onDeleteButton} color="red" radius="lg">
+          <Button
+            onClick={() => onDeleteButton(deleteData)}
+            color="red"
+            radius="lg"
+          >
             Delete
           </Button>
-          <Button onClick={onClose} color="gray" radius="lg">
+          <Button onClick={actionModalClose} color="gray" radius="lg">
             Cancel
           </Button>
         </div>
